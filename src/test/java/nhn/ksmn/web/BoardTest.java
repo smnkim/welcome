7bdafa00343bca8d94d723c0c09418a42e2f68ab
smn Kim <kimseamina@naver.com>
2015-12-31 16:49:46 +0900

    jUnitTest_#2 and Finish

--- src/test/java/nhn/ksmn/web/BoardTest.java
+++ src/test/java/nhn/ksmn/web/BoardTest.java
@@ -3,25 +3,30 @@ package nhn.ksmn.web;
 import static org.junit.Assert.fail;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import nhn.ksmn.web.board.entity.Board;
-import nhn.ksmn.web.board.service.impl.BoardServiceImpl;
+import nhn.ksmn.web.board.service.BoardService;
 import nhn.ksmn.web.board.util.EmailValidator;
 
 public class BoardTest {
-	@Autowired
-	BoardServiceImpl boardService;
-	
-	Board board;
-	
+	private BoardService boardService;
+
+	@Before
+	public void initData() {
+		ApplicationContext factory = new ClassPathXmlApplicationContext("root-context.xml");
+		boardService = (BoardService) factory.getBean("boardService");
+	}
+
 	@Test
 	public void testRegisterimpl() throws Exception {
-//		
-//		Board newBoard = new Board("smnkim@nhnent.com","123","게시판 제목","본문의 내용을 작성하고 있습니다.");
-//		int actual = boardService.insert(newBoard);
-//		Assert.assertEquals(actual,1);
+		
+		Board newBoard = new Board("smnkim@nhnent.com","123","게시판 제목","본문의 내용을 작성하고 있습니다.");
+		int actual = boardService.insert(newBoard);
+		Assert.assertEquals(actual,1);
 	}
 	
 	@Test
@@ -32,6 +37,7 @@ public class BoardTest {
 //		String[] email = new String[] { "smnkim@test.c111111om",
 //				"smnkim@n22.22.222hn.ent", "smn@#$.kim@nhnent.com",
 //				"testemail@navercom", "kimseamina@naver.com" };
+		
 		String[] email = new String[] { "smnkim@test.com",
 				"smnkim@nhn.ent", "smn.kim@nhnent.com",
 				"testemail@naver.com", "kimseamina@naver.com" };
@@ -47,22 +53,26 @@ public class BoardTest {
 	public void testMyboard() throws Exception {
 		Board newBoard = (Board) boardService.select(new Board(1));
 		Board findBoard = (Board) boardService.select(new Board(newBoard.getNo()));
-		Assert.assertEquals(newBoard, findBoard);
+		
+		Boolean result = (newBoard.getNo()==findBoard.getNo()?true:false);
+		System.out.println(newBoard.toString());
+		Assert.assertEquals(result, true);
 	}
 
 	@Test
-	public void testConfirmpwdimpl() {
-		fail("Not yet implemented");
+	public void testConfirmpwdimpl() throws Exception {
+
+		Board newBoard = new Board(2);
+		Board getBoard = (Board) boardService.select(newBoard);
+		
+		boolean result = ((getBoard.getPwd() != null && getBoard.getPwd().equals("1"))?true:false);
+		Assert.assertEquals(result, true);
 	}
 
 	@Test
-	public void testUpdateimpl() {
-		fail("Not yet implemented");
+	public void testUpdateimpl() throws Exception {
+		Board newBoard = new Board(9, "업데이트테스할거얌!!!!!!!!!", "본문 내용 바뀌니~~~");
+		int actual = boardService.update(newBoard);
+		Assert.assertEquals(actual,1);
 	}
-
-	@Test
-	public void testSelectallajax() {
-		fail("Not yet implemented");
-	}
-
 }
